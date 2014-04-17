8d792ef367f2fff1d5cdebab49919df4a0bff79c
Miroslav Bendík <bendik@creanet.sk>
2014-04-17 09:02:06 +0200

    Simplified code.

--- tastypie_browser/resources.py
+++ tastypie_browser/resources.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 def patch_resource():
-	from tastypie.api import Api
 	from tastypie.resources import Resource
-	from tastypie.utils.mime import determine_format, build_content_type
 	from django.http.response import HttpResponse
 
 	old_serialize = Resource.serialize
 	setattr(Resource, "old_serialize", old_serialize)
 
-	def serialize(self, request, data, format, options=None):
+	def serialize(self, request, data, format, options=None): # pylint: disable=W0622
 		if format == 'text/html':
 			options = options or {}
 			options['request'] = request
@@ -19,39 +17,3 @@ def patch_resource():
 		return self.old_serialize(request, data, format, options)
 
 	setattr(Resource, "serialize", serialize)
-
-	old_top_level = Api.top_level
-	setattr(Api, "old_top_level", old_top_level)
-
-	def top_level(self, request, api_name=None):
-		desired_format = determine_format(request, self.serializer)
-		if 'text/html' in desired_format:
-			available_resources = {}
-
-			if api_name is None:
-				api_name = self.api_name
-
-			for name in sorted(self._registry.keys()):
-				available_resources[name] = {
-					'list_endpoint': self._build_reverse_url("api_dispatch_list", kwargs={
-						'api_name': api_name,
-						'resource_name': name,
-					}),
-					'schema': self._build_reverse_url("api_get_schema", kwargs={
-						'api_name': api_name,
-						'resource_name': name,
-					}),
-				}
-
-			options = {
-				'request': request,
-				'to_html': True,
-				'api': self,
-			}
-			options['response'] = HttpResponse(content=self.serializer.serialize(available_resources, "application/json", options), content_type="application/json")
-			serialized = self.serializer.serialize(available_resources, desired_format, options)
-			return HttpResponse(content=serialized, content_type=build_content_type(desired_format))
-		else:
-			return self.old_top_level(request, api_name)
-
-	setattr(Api, "top_level", top_level)
--- tastypie_browser/serializers.py
+++ tastypie_browser/serializers.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
-from django.template.loader import render_to_string
+import re
+
 from django.core.serializers import json as djangojson
 from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
 from django.utils.html import escape
-import re
+from tastypie import api
+from tastypie.serializers import Serializer
+
+
+class ApiSerializer(Serializer):
+	def to_html(self, data, options=None):
+		options = options or {}
+		options['api'] = getattr(self, "api")
+		return super(ApiSerializer, self).to_html(data, options)
+
+
+class HtmlApi(api.Api):
+	def __init__(self, api_name="v1", serializer_class=ApiSerializer):
+		super(HtmlApi, self).__init__(api_name=api_name, serializer_class=serializer_class)
+		setattr(self.serializer, 'api', self)
 
 
 def patch_serializer():
-	from tastypie.serializers import Serializer
+	link_re = re.compile(r'^(\s+&quot;[\w_-]+&quot;: &quot;)(/[a-z0-9/_-]+)(&quot;)', flags=re.MULTILINE)
 
 	def to_json(self, data, options=None):
 		options = options or {}
@@ -16,7 +32,6 @@ def patch_serializer():
 		if to_html:
 			indent = 2
 		data = self.to_simple(data, options)
-
 		data = djangojson.json.dumps(data, cls=djangojson.DjangoJSONEncoder, sort_keys=True, ensure_ascii=False, indent=indent)
 		return data
 
@@ -31,9 +46,10 @@ def patch_serializer():
 		resource = options['resource']
 		request = options['request']
 		content = escape(options['response'].content)
-		content = re.sub(r'^(\s+&quot;[\w_-]+&quot;: &quot;)(/[a-z0-9/_-]+)(&quot;)', r'\1<a href="\2">\2</a>\3', content, flags=re.MULTILINE)
-		api_name = resource._meta.api_name
-		resource_name = resource._meta.resource_name
+		content = link_re.sub(r'\1<a href="\2">\2</a>\3', content)
+		resource_options = getattr(resource, "_meta")
+		api_name = resource_options.api_name
+		resource_name = resource_options.resource_name
 
 		path = request.path
 		list_path = resource.get_resource_uri()
@@ -49,9 +65,10 @@ def patch_serializer():
 			'request': request,
 			'response': options['response'],
 			'name': resource_name,
-			'allowed_methods': [m.upper() for m in resource._meta.allowed_methods],
+			'allowed_methods': [m.upper() for m in resource_options.allowed_methods],
 			'available_formats': self.formats,
 			'breadcrumblist': breadcrumblist,
+			'data': data
 		}
 		templates = (
 			"tastypie_browser/api_{0}.html".format(resource_name),
@@ -61,22 +78,20 @@ def patch_serializer():
 
 	def toplevel_to_html(self, data, options=None):
 		options = options or {}
-		request = options['request']
-		content = escape(options['response'].content)
-		content = re.sub(r'^(\s+&quot;[\w_-]+&quot;: &quot;)(/[a-z0-9/_-]+)(&quot;)', r'\1<a href="\2">\2</a>\3', content, flags=re.MULTILINE)
-		api_name = options['api'].api_name
+		api_instance = options['api']
+		api_name = api_instance.api_name
+		serializer = api_instance.serializer
 
 		breadcrumblist = []
 		breadcrumblist.append(('API {0}'.format(api_name), reverse("api_{0}_top_level".format(api_name), kwargs={'api_name': api_name})))
 
+		content = escape(serializer.serialize(data, 'application/json', {'to_html': True}))
+		content = self.link_re.sub(r'\1<a href="\2">\2</a>\3', content)
 		ctx = {
-			'content': content,
-			'request': request,
-			'response': options['response'],
 			'name': api_name,
-			'allowed_methods': ['get'],
-			'available_formats': self.formats,
 			'breadcrumblist': breadcrumblist,
+			'data': data,
+			'content': content,
 		}
 		templates = (
 			"tastypie_browser/toplevel.html",
@@ -84,12 +99,9 @@ def patch_serializer():
 		)
 		return render_to_string(templates, ctx)
 
-
-	def from_html(self, content):
-		pass
-
 	setattr(Serializer, "to_json", to_json)
 	setattr(Serializer, "to_html", to_html)
 	setattr(Serializer, "resource_to_html", resource_to_html)
 	setattr(Serializer, "toplevel_to_html", toplevel_to_html)
-	setattr(Serializer, "from_html", from_html)
+	setattr(Serializer, "link_re", link_re)
+	setattr(api, "Api", HtmlApi)
